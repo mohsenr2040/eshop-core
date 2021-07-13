37998d4f069dc3e47305af88bdc7e9319069346a
mohsen <mohsenr2020@gmail.com>
2021-07-13 13:09:31 +0430

    implement actions of users comments in mongoDb

--- EndPoint.Site/Controllers/ProductsController.cs
+++ EndPoint.Site/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EndPoint.Site.Utilities;
 using eshop.Application.Interfaces.FacadPatterns;
+using eshop.Application.Services.MongoDb;
 using eshop.Application.Services.Products.Queries.GetProductForSite;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -13,10 +15,12 @@ namespace EndPoint.Site.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductFacad _productFacad;
+        private readonly CommentService _commentService;
       
-        public ProductsController(IProductFacad productFacad)
+        public ProductsController(IProductFacad productFacad, CommentService commentService)
         {
             _productFacad = productFacad;
+            _commentService = commentService;
         }
         //[Route("Index")]
         public IActionResult Index(Ordering ordering, string SearchKey, int? CatId = null, int Page = 1, int PageSize = 20)
@@ -24,10 +28,16 @@ namespace EndPoint.Site.Controllers
             return View(_productFacad.GetProductForSiteService.Execute(SearchKey, CatId, Page, PageSize, ordering).Data);
         }
 
-        
+
         [HttpGet("{ProductId}-pka/{ProductName}/{SellerId?}")]
         public IActionResult Detail( int ProductId, int? SellerId=null,string pka="-pka", string ProductName="")
         {
+            try
+            {
+                ViewBag.Comments = _commentService.Get(ProductId.ToString()).Data;
+            }
+            catch { }
+
             if (SellerId==null)
             {
                 return View(_productFacad.GetProductDetailForSiteService.Execute(ProductId).Data);
@@ -37,6 +47,24 @@ namespace EndPoint.Site.Controllers
                 return View(_productFacad.GetProductDetailForSiteService.Execute(ProductId, SellerId).Data);
 
             }
+        }
+
+        [HttpPost]
+        [Route("Products/RegisterComment")]
+        public IActionResult RegsterComment(string Text,string ProductId)
+        {
+            string UserId = ClaimUtility.GetUserId(HttpContext.User);
+            string UserFullName = ClaimUtility.GetUserFullName(HttpContext.User);
+
+            return Json(_commentService.Create(ProductId, Text, UserId, UserFullName));
+        }
+
+        [HttpPost]
+        [Route("Products/AddReply")]
+        public IActionResult AddReply(string commentId, string text)
+        {
+            string UserFullName = ClaimUtility.GetUserFullName(HttpContext.User);
+            return Json(_commentService.AddReply(commentId, UserFullName, text));
         }
       
     }
