0e0c154a0b3ba76c486f3727e52626409bd2e097
mohsen <mohsenr2020@gmail.com>
2021-07-14 09:58:27 +0430

    Add comment.cs class

---
+++ eshop.Domain/Entities/MongoDb/Comment.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+using System.Collections.Generic;
+
+namespace eshop.Domain.Entities.MongoDb
+{
+    public class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string UserId { get; set; }
+        public string user { get; set; }
+        public string text { get; set; }
+        public List<Reply> Replies { get; set; }
+    }
+    public class Reply
+    {
+        public string UserId { get; set; }
+        public string user { get; set; }
+        public string text { get; set; }
+    }
+}
